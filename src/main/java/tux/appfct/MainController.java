34c182756d45812a1c17097aecb19819acc93a6e
Fernando <store.nthing@gmail.com>
2023-04-01 16:11:07 -0300

    MainController

--- src/main/java/tux/appfct/MainController.java
+++ src/main/java/tux/appfct/MainController.java
@@ -1,14 +1,38 @@
 package tux.appfct;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
 
-public class MainController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private MenuItem menuItemClient;
+
+    @FXML
+    private MenuItem menuItemPersonal;
+
+    @FXML
+    private MenuItem menuSobre;
+
+    @FXML
+    public void  onMenuItemClientAction(){
+        System.out.println("onMenuItemClientAction");
+    }
+
+    @FXML
+    public void  onMenuItemPersonalAction(){
+        System.out.println("onMenuItemPersonalAction");
+    }
+
+    @FXML
+    public void  onMenuItemSobreAction(){
+        System.out.println("onMenuItemSobreAction");
+    }
+    @Override
+    public void initialize(URL uri, ResourceBundle rb) {
     }
 }
